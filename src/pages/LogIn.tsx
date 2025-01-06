140992268ca27c66f011c74fee89475e2546a15a
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-01-06 16:14:17 +0000

    [Chore] SC-162578 SC-123527 upgrade deps

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -40,16 +40,10 @@ const LogInPage: FC = () => {
         client.deregisterElement("githubMenu");
     }, [client]);
 
-    const selfHosted=true;
     useEffect(() => {
         if (!callback) {
-            if (selfHosted === true) {
-                client?.oauth2().getGenericCallbackUrl(key, /code=(?<token>[0-9a-f]+)/, /state=(?<key>.+)/)
-                    .then(setCallback);
-            } else {
-                client?.oauth2().getGenericCallbackUrl(key, /code=(?<token>[0-9a-f]+)/, /state=(?<key>.+)/)
-                    .then(setCallback);
-            }
+            client?.oauth2().getGenericCallbackUrl(key, /code=(?<token>[0-9a-f]+)/, /state=(?<key>.+)/)
+                .then(setCallback);
         }
     }, [client, key, callback]);
 
