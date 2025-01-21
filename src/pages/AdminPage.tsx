1409921cc2701bfe22c9071ed1f629ea5a93dad7
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-01-21 16:45:14 +0000

    [Feature] SC-167944 Update Apps to use Global Proxy Service

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useMemo } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
